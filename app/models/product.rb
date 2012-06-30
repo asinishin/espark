322f46553df29e715d4e942c3eec6c77b000ed95
Alex Sinishin <sinishin@gmail.com>
2012-06-30 12:00:51 +0300

    First test of real API

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,38 +1,60 @@
 class Product
   attr_accessor :track_id, :track_name, :artwork_url_60
   
-  def initialize(json=nil)
+  def initialize(attrs=nil)
     super()
-    unless json.nil?
-      attrs = JSON.parse(json)
-      @track_id = attrs['trackId']
-      @track_name = attrs['trackName']
-      @artwork_url_60 = attrs['artworkUrl60']
+    unless attrs.nil?
+      @track_id       = attrs[:track_id]
+      @track_name     = attrs[:track_name]
+      @artwork_url_60 = attrs[:artwork_url_60]
+    end
+  end
+
+  def self.build_from_json(json)
+    apps_data = JSON.parse(json)
+    unless apps_data['resultCount'].nil?
+      apps_data = apps_data['results']
+      apps_data.map do |attrs|
+        self.new(
+          track_id:       attrs['trackId'],
+          track_name:     attrs['trackName'],
+          artwork_url_60: attrs['artworkUrl60']
+        )
+      end
+    else
+      []
     end
   end
 
   def self.build_dummies
-    apps = []
-    apps << new({
-      trackId: '456191378',
-      trackName: 'forkly',
-      artworkUrl60: 'http://a2.mzstatic.com/us/r1000/116/Purple/v4/da/75/b8/da75b8b8-e070-e7c0-805f-10f5f37123a7/Icon.png'
-    }.to_json)
-    apps << new({
-      trackId: '284945674',
-      trackName: 'iWant',
-      artworkUrl60: 'http://a5.mzstatic.com/us/r1000/105/Purple/89/10/55/mzi.rszguyzr.png'
-    }.to_json)
-    apps << new({
-      trackId: '449991112',
-      trackName: 'YellowPages - Mobile Yellow Pages',
-      artworkUrl60: 'http://a2.mzstatic.com/us/r1000/065/Purple/70/d3/10/mzi.watmijse.png'
-    }.to_json)
-    apps << new({
-      trackId: '284910350',
-      trackName: 'Yelp',
-      artworkUrl60: 'http://a2.mzstatic.com/us/r1000/073/Purple/v4/ec/ba/57/ecba5736-8bf9-2f5f-fced-02d49ec15d41/57.png'
-    }.to_json)
+    apps_data = [
+      {
+        trackId: '456191378',
+        trackName: 'forkly',
+        artworkUrl60: 'http://a2.mzstatic.com/us/r1000/116/Purple/v4/da/75/b8/da75b8b8-e070-e7c0-805f-10f5f37123a7/Icon.png'
+      },
+      {
+        trackId: '284945674',
+        trackName: 'iWant',
+        artworkUrl60: 'http://a5.mzstatic.com/us/r1000/105/Purple/89/10/55/mzi.rszguyzr.png'
+      },
+      {
+        trackId: '449991112',
+        trackName: 'YellowPages - Mobile Yellow Pages',
+        artworkUrl60: 'http://a2.mzstatic.com/us/r1000/065/Purple/70/d3/10/mzi.watmijse.png'
+      },
+      {
+        trackId: '284910350',
+        trackName: 'Yelp',
+        artworkUrl60: 'http://a2.mzstatic.com/us/r1000/073/Purple/v4/ec/ba/57/ecba5736-8bf9-2f5f-fced-02d49ec15d41/57.png'
+      }
+    ]
+    apps_data = {
+      resultCount: apps_data.size,
+      results:     apps_data
+    }
+    build_from_json(apps_data.to_json)
+#    apps_data.to_json
   end
 
   def self.dummy_search_by_name(track_name)
--- app/models/store_api.rb
+++ app/models/store_api.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class StoreApi
   def self.search_by_name(app_name)
     Product.dummy_search_by_name(app_name)
@@ -8,6 +10,9 @@ class StoreApi
   end
 
   def self.lookup_by_id(app_id)
-    Product.dummy_search_by_id(app_id)
+#    Product.dummy_search_by_id(app_id)
+    uri = URI.parse("http://itunes.apple.com/lookup?id=#{app_id}")
+    json = Net::HTTP.get(uri)
+    Product.build_from_json(json).first
   end
 end
