7a04f33a888806d4c554b10ee4e586fa4555ad69
Alex Sinishin <sinishin@gmail.com>
2012-07-03 18:21:16 +0300

    Added name index to tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120703050435) do
+ActiveRecord::Schema.define(:version => 20120703151800) do
 
   create_table "batches", :force => true do |t|
     t.string   "example",    :limit => 30
@@ -41,5 +41,7 @@ ActiveRecord::Schema.define(:version => 20120703050435) do
     t.datetime "created_at",               :null => false
     t.datetime "updated_at",               :null => false
   end
+
+  add_index "tags", ["name"], :name => "index_tags_on_name", :unique => true
 
 end
