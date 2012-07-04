1069445476c153cc160b04738c04d4195969ec3f
Alex Sinishin <sinishin@gmail.com>
2012-07-04 13:44:02 +0300

    Fixed search field layout

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,5 +3,10 @@ module ProductsHelper
   def test?
     ::Rails.env == 'test'
   end
+  
+  def no_pics?
+     @product.screenshot_urls.nil? || \
+     @product.screenshot_urls.empty? || test?
+  end
 
 end
