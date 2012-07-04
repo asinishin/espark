0b10f84c70f90b0164704795bfc58ef05cf27f6d
Alex Sinishin <sinishin@gmail.com>
2012-07-04 03:01:00 +0300

    Added check for uniqueness of Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,6 +4,9 @@ class Tag < ActiveRecord::Base
   attr_accessible :name, :tag_id
 
   before_create :normalize
+  validates_presence_of   :name, :tag_id
+  validates_uniqueness_of :name
+  validates_uniqueness_of :tag_id
 
   def self.search_by_example(name_example)
     where("name ilike '#{name_example}%'").order('name').first(8)
--- lib/globals.rb
+++ lib/globals.rb
@@ -1,3 +1,3 @@
 module Globals
-  TAG_NAME_LENGTH = 30
+  TAG_NAME_LENGTH = 28
 end
